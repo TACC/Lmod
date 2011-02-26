67c3ad7d73efd391ccad0a87db57fbc5742832b6
Robert McLay <mclay@zaniahgroup.com>
2011-02-25 19:57:13 -0600

    forcing oscar to search the directory alphabetically

---
+++ rt/oscar/mf/Core/oscar.lua
@@ -0,0 +1,21 @@
+help("\tThis module sets up the OSCAR modules subsystem.")
+whatis("Description: Sets up the OSCAR modules subsystem.")
+local mroot = os.getenv("MODULEPATH_ROOT")
+local oscarDir = pathJoin(mroot,"Oscar")
+prepend_path("MODULEPATH", oscarDir)
+
+local a = {}
+
+for file in lfs.dir(oscarDir) do
+   if (file ~= "." and file ~= ".." and file:sub(-1,-1) ~= '~') then
+      a[#a+1] = file
+   end
+end
+
+table.sort(a)
+
+for i = 1,#a do
+   load(a[i])
+end
+
+     
