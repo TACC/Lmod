f12830e56124db28fe1512645ae08b54436caf13
Robert McLay <mclay@zaniahgroup.com>
2011-04-18 17:10:31 -0500

    machine portable cron job for collecting module usage data

--- src/cron_module_usage.sh
+++ src/cron_module_usage.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
+HOST=$(hostname -f)
+TACC_ADMIN_DIR_ranger=/share/tacc_admin/moduleUsage
+TACC_ADMIN_DIR_ls4=/home1/tacc_admin/moduleUsage
+TACC_ADMIN_DIR_longhorn=/share/tacc_admin/moduleUsage
+NAME=$(expr "$HOST" : '[^.][^.]*\.\(.*\)\.tacc.utexas.edu')
+
+eval "TACC_ADMIN_DIR=\$TACC_ADMIN_DIR_$NAME"
+
 DATE=$(date +"%F")
-Fn="moduleUsage-$DATE.csv"
+Fn="$TACC_ADMIN_DIR/moduleUsage-$DATE.csv"
 
 PATH=/opt/apps/lmod/lmod/libexec:$PATH
 
-echo processMT -d -f $Fn
 processMT -d -f $Fn
-
-
-
