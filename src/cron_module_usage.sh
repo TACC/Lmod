ce8c157d7152ee08cc85f1ce56095801b3b672d9
Robert McLay <mclay@zaniahgroup.com>
2011-04-21 16:55:54 -0500

    support for grace added

--- src/cron_module_usage.sh
+++ src/cron_module_usage.sh
@@ -3,6 +3,7 @@ HOST=$(hostname -f)
 TACC_ADMIN_DIR_ranger=/share/tacc_admin/moduleUsage
 TACC_ADMIN_DIR_ls4=/home1/tacc_admin/moduleUsage
 TACC_ADMIN_DIR_longhorn=/share/tacc_admin/moduleUsage
+TACC_ADMIN_DIR_csr=/home/tacc/tacc_admin/moduleUsage
 NAME=$(expr "$HOST" : '[^.][^.]*\.\(.*\)\.tacc.utexas.edu')
 
 eval "TACC_ADMIN_DIR=\$TACC_ADMIN_DIR_$NAME"
