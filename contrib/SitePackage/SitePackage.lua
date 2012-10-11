7b0f0de48c920128c4fb91e3d5d53d7eb69c2f70
Robert McLay <mclay@zaniahgroup.com>
2012-10-10 20:34:46 -0500

    added sitePackage loading in spider.in and computeHash.in

--- contrib/SitePackage/SitePackage.lua
+++ contrib/SitePackage/SitePackage.lua
@@ -1,9 +1,11 @@
 -- -*- lua -*-
 -- vim:ft=lua:et:ts=4
-
+local Dbg          = require("Dbg")
 sitePkgRoot = os.getenv("LMOD_PKG_ROOT") or "/global/apps"
 
 function checkRestrictedGroup(pkg, group) 
+   local dbg = Dbg:dbg()
+   dbg.start("checkRestrictedGroup(pkg, \"",tostring(group),"\")")
    if (mode() ~= "load") then return true end
    if (group == nil)     then return true end
    local err_message = "Only users in group \'" .. group .. 
@@ -13,18 +15,23 @@ function checkRestrictedGroup(pkg, group)
 
    for g in grps:split("[ \n]") do
       if (g == group) then
+         dbg.fini()
          return true
       end
    end
    LmodError(err_message,"\n")
+   dbg.fini()
    return false
 end
 
 function logUsage(pkg)
+   local dbg = Dbg:dbg()
+   dbg.start("logUsage(pkg)")
    if (mode() ~= "load") then return true end
    local user = os.getenv("USER")
    local jobid = os.getenv("PBS_JOBID")
    local msg = ""
+   dbg.print("user: ",user," jobid: ",jobid,"\n")
    if jobid == nil then 
       msg = string.format("user=%s,app=%s", user, pkg.id)
    else
@@ -33,6 +40,7 @@ function logUsage(pkg)
    end
    local cmd = "logger -t lmod -p local0.info " .. msg
    os.execute(cmd)
+   dbg.fini()
 end
 
 function prependModulePath(subdir)
