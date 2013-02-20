2bd7562e88b7faf694acfaba4c47a08502fe067d
Robert McLay <mclay@zaniahgroup.com>
2013-02-20 14:49:28 -0600

    more multiL foo

--- rt/multiL/mf/cmplr/gcc/4.7/mpi/mpich2/1.5.2.lua
+++ rt/multiL/mf/cmplr/gcc/4.7/mpi/mpich2/1.5.2.lua
@@ -1,6 +1,6 @@
 -- -*- lua -*-
 local pkgName         = "mpi/mpich2"
-local cmplrV          = "cmplr/gcc/4.7.2"
+local cmplrV          = "cmplr/gcc/4.7"
 local fn              = myFileName():gsub("%.lua$","")
 local fullVersion     = barefilename(fn)
 local pkgVersion      = fullVersion:match("([0-9]+%.[0-9]+)%.?")
@@ -9,7 +9,7 @@ local modulepath_root = os.getenv("MODULEPATH_ROOT")
 
 
 
-prepend_path('MODULEPATH',           pathJoin(modulepath_root,pkgNameVer,cmplrV,))
+prepend_path('MODULEPATH',           pathJoin(modulepath_root,pkgNameVer,cmplrV))
 
 family("compiler")
 
