bd967ff41959ba45623037750bd492fc526a8be0
Robert McLay <mclay@zaniahgroup.com>
2013-03-08 17:14:09 -0600

    how to handle links

--- src/MT.lua
+++ src/MT.lua
@@ -1158,7 +1158,7 @@ end
 
 function M.serializeTbl(self)
    local dbg = Dbg:dbg()
-   dbg.print("s_mt.short: ", tostring(s_mt.shortTime),"\n")
+   dbg.print("s_mt.shortTime: ", tostring(s_mt.c_shortTime),"\n")
    
    s_mt.activeSize = self:setLoadOrder()
 
