bc58fcf10aeea3ed5d3289b084fd9f97f7db6734
Robert McLay <mclay@zaniahgroup.com>
2013-03-30 21:31:12 -0500

    mother may i added

---
+++ contrib/hostTypeCacheBuilder/clusterDescriptT.lua
@@ -0,0 +1,10 @@
+clusterDescriptT = {
+   login = {'staff', 'login1', 'login2', 'login3', 'login4', 'login5',},
+   bigmem = {'c400-116',},
+   compute_devel = {'c557-901',},
+   compute = {'c486-101'},
+   vis = {'c443-901'},
+}
+
+
+
