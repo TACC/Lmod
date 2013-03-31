b33cd0d30c3ae58c8d846f67ecc4738abac32897
Robert McLay <mclay@zaniahgroup.com>
2013-03-31 11:14:34 -0500

    update comments

--- contrib/hostTypeCacheBuilder/clusterDescriptT.lua
+++ contrib/hostTypeCacheBuilder/clusterDescriptT.lua
@@ -1,9 +1,12 @@
 clusterDescriptT = {
-   login         = {'staff', 'login1', 'login2', 'login3', 'login4', 'login5',},
-   bigmem        = {'c400-116',},
-   compute_devel = {'c557-901',},
-   compute       = {'c486-101'},
-   vis           = {'c443-901'},
+   ping_options = "-c 1 -w 1",
+   hostType = {
+      login         = {'staff', 'login1', 'login2', 'login3', 'login4', 'login5',},
+      bigmem        = {'c400-116',},
+      compute_devel = {'c557-901',},
+      compute       = {'c486-101'},
+      vis           = {'c443-901'},
+   }
 }
 
 
