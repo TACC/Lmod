68201f9dde310836abf44102ff126cd3bbe4d20f
Robert McLay <mclay@zaniahgroup.com>
2013-04-10 16:47:43 -0500

    moving to TAG_VERSION 5.0b6

--- src/Version.lua
+++ src/Version.lua
@@ -1,4 +1,4 @@
 --module('Version')
 local M={}
-function M.name() return "5.0b6 2013-04-10 16:40 ..GIT_VERSION" end
+function M.name() return "5.0b6 ("..GIT_VERSION..") 2013-04-10 16:47" end
 return M
