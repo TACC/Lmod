ee3bb74d41bb27dd70acb629570411154818a974
Robert McLay <mclay@zaniahgroup.com>
2013-04-11 11:01:15 -0500

    quiet cache file creation when using spider command

--- src/Cache.lua
+++ src/Cache.lua
@@ -168,7 +168,7 @@ local function readCacheFile(self, cacheFileA)
                local G_moduleT = _G.moduleT
                for k, v in pairs(G_moduleT) do
                   if ( k:sub(1,1) == '/' ) then
-                     local dirTime = moduleDirT[k]
+                     local dirTime = moduleDirT[k] or 0
                      if (attr.modification > dirTime) then
                         dbg.print("saving directory: ",k," from cache file: ",f,"\n")
                         moduleDirT[k] = attr.modification
