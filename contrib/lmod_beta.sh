4ad669ceb9a8b4f59a023f7937b0325205b51be0
Robert McLay <mclay@zaniahgroup.com>
2013-04-17 13:54:39 -0500

    updated completion foo

--- contrib/lmod_beta.sh
+++ contrib/lmod_beta.sh
@@ -1,22 +1,21 @@
 MCLAY=~mclay
 
 for i in $MCLAY/l/pkg/$(uname -m) $MCLAY/l/pkg; do
-  if [ -d $i/luatools ]; then  
+  if [ -x $i/lmod/lmod/libexec/lmod ]; then  
     PKG_DIR=$i
     break
   fi
 done
 
-export LUA_PATH="$PKG_DIR/luatools/luatools/share/5.1/?.lua;;"
-export LUA_CPATH="$PKG_DIR/luatools/luatools/lib/5.1/?.so;;"
-
-export LMOD_CMD=$PKG_DIR/lmod/lmod/libexec/lmod
-ML_DIR=$PKG_DIR/lmod/lmod/libexec/
+#export LUA_PATH="$PKG_DIR/luatools/luatools/share/5.1/?.lua;;"
+#export LUA_CPATH="$PKG_DIR/luatools/luatools/lib/5.1/?.so;;"
+export LMOD_DIR=$PKG_DIR/lmod/lmod/libexec
+export LMOD_CMD=$LMOD_DIR/lmod
 
 export BASH_ENV=$PKG_DIR/lmod/lmod/init/bash
 
 ml () {
-  eval $($ML_DIR/ml_cmd "$@")
+  eval $($LMOD_DIR/ml_cmd "$@")
 }
 
 module () {
@@ -26,4 +25,4 @@ module () {
 
 module purge
 clearMT
-module getdefault default || module try-add TACC
+module --initial_load restore
