c291f821552ed3d6ba1cc770b5f9490614fff1d5
Robert McLay <mclay@zaniahgroup.com>
2013-05-05 13:46:59 -0500

    formating issues with uitSitePkg test

--- rt/uitSitePkg/mf/Core/gcc/4.7.2.lua
+++ rt/uitSitePkg/mf/Core/gcc/4.7.2.lua
@@ -4,13 +4,8 @@
 family("compiler")
 local pkg = loadPkgDefaults()
 setPkgInfo(pkg)
-
-setPkgInfo(pkg)
-checkRestrictedGroup(pkg, nil)
 prependModulePath(pathJoin("Compiler", pkg.id))
-logUsage(pkg)
-
-append_path("MANPATH", pathJoin(pkg.prefix, "share/man"))
-prepend_path("PATH", pathJoin(pkg.prefix, "bin"))
+prepend_path("MANPATH",         pathJoin(pkg.prefix, "share/man"))
+prepend_path("PATH",            pathJoin(pkg.prefix, "bin"))
 prepend_path("LD_LIBRARY_PATH", pathJoin(pkg.prefix, "lib64"))
-prepend_path("LIBRARY_PATH", pathJoin(pkg.prefix, "lib64"))
+prepend_path("LIBRARY_PATH",    pathJoin(pkg.prefix, "lib64"))
