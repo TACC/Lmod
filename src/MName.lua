f5e7db79132c6c510733859c7f0aee567be34f84
Robert McLay <mclay@zaniahgroup.com>
2013-05-20 21:57:11 -0500

    Added Comment to MName

--- src/MName.lua
+++ src/MName.lua
@@ -32,11 +32,48 @@
 --
 --------------------------------------------------------------------------
 
+--------------------------------------------------------------------------
+-- MName: This class manages module names.  It turns out that a module
+--        name is more complicated only Lmod started supporting
+--        category/name/version style module names.  Lmod automatically
+--        figures out what the "name", "full name" and "version" are.
+--        The "MT:locationTbl()" knows the 3 components for modules that
+--        can be loaded.  On the other hand, "MT:exists()" knows for
+--        modules that are already loaded.
+
+--        The problem is when a user gives a module name on the command
+--        line.  It can be the short name or the full name.  The trouble
+--        is that if the user gives "foo/bar" as a module name, it is
+--        quite possible that "foo" is the name and "bar" is the version
+--        or "foo/bar" is the short name.  The only way to know is to
+--        consult either choice above.
+--
+--        Yet another problem is that a module that is loaded may not be
+--        in the module may not be available to load because the
+--        MODULEPATH has changed.  Or if you are loading a module then it
+--        must be in the locationTbl.  So clients using this class must
+--        specify to the ctor that the name of the module is one that will
+--        be loaded or one that has been loaded.
+--
+--        Another consideration is that Lmod only allows for one "name"
+--        to be loaded at a time.
+
 require("strict")
 
 local M   = {}
 local Dbg = require("Dbg")
 local MT  = require("MT")
+
+--------------------------------------------------------------------------
+-- shorten(): This function allows for taking the name and remove one
+--            level at a time.  Lmod rules require that if a module is
+--            loaded or available, that the "short" name is either
+--            the name given or one level removed.  So checking for
+--            a "a/b/c/d" then the short name is either "a/b/c/d" or
+--            "a/b/c".  It can't be "a/b" and the version be "c/d".
+--            In other words, the "version" can only be one component,
+--            not a directory/file.  This function can only be called
+--            with level = 0 or 1.
 
 local function shorten(name, level)
    if (level == 0) then
@@ -47,6 +84,15 @@ local function shorten(name, level)
    j = (j or 0) - 1
    return name:sub(1,j)
 end
+
+--------------------------------------------------------------------------
+-- MName:new(): This ctor takes "sType" to lookup in either the
+--              locationTbl() or the exists() depending on whether it is
+--              "load" for modules to be loaded (available) or it is
+--              already loaded.  Knowing the short name it is possible to
+--              figure out the version (if one exists).  If the module name
+--              doesn't exist then the short name (sn) and version are set 
+--              to false.
 
 function M.new(self, sType, name)
    local o = {}
@@ -85,7 +131,7 @@ function M.new(self, sType, name)
             break
          end
       end
-   end
+    end
 
    if (sn) then
       o._sn      = sn
@@ -96,13 +142,24 @@ function M.new(self, sType, name)
    return o
 end
 
+--------------------------------------------------------------------------
+-- MName:sn(): Return the short name
+
 function M.sn(self)
    return self._sn
 end
 
+--------------------------------------------------------------------------
+-- MName:usrName(): Return the user specified name.  It could be the
+--                  short name or the full name.
+
 function M.usrName(self)
    return self._name
 end
+
+--------------------------------------------------------------------------
+-- MName:version(): Return the version for the module.  Note that the
+--                  version is nil if not known.
 
 function M.version(self)
    return self._version
