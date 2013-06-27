8ac45a2d94af35d8197a69b6099d07652535abb7
Robert McLay <mclay@zaniahgroup.com>
2013-06-26 20:00:23 -0500

    updating getUname to include cpu model info

--- settarg/getUname.lua
+++ settarg/getUname.lua
@@ -13,6 +13,7 @@ function getUname()
       return t
    end
 
+   local machFullName     = nil
    local osName           = posix.uname("%s")
    local machName         = posix.uname("%m")
    osName                 = string.gsub(osName,"[ /]","_")
@@ -22,11 +23,39 @@ function getUname()
       osName   = "Irix"
       machName = "mips"
    end
-   t.osName    = osName
-   t.machName  = machName
-   t.os_mach   = osName .. '-' .. machName
-   t.target    = os.getenv("TARGET") or ""
-   t.hostName  = capture("hostname -f"):gsub("%s+","")
+   if (osName == "Linux") then
+      local cpu_family
+      local model
+      local count = 0
+      local f = io.open("/proc/cpuinfo","r")
+      if (f) then
+         while (true) do
+            local line = f:read("*line")
+            if (line == nil) then break end
+            if (line:find("cpu family")) then
+               local _, _, v = line:find(".*:%s*(.*)")
+               cpu_family = string.format("%02x",v)
+               count = count + 1
+            else if (line:find("model name")) then
+               local _, _, v = line:find(".*:%s*(.*)")
+               machFullName = string.format("%02x",v)
+               count = count + 1
+            else if (line:find("model")) then
+               local _, _, v = line:find(".*:%s*(.*)")
+               model = string.format("%02x",v)
+               count = count + 1
+            end
+            if (count > 2) then break end
+         end
+      end
+      machName = machName .. "_" .. cpu_family .. "_" .. model
+   end
+   t.osName       = osName
+   t.machName     = machName
+   t.machFullName = machFullName
+   t.os_mach      = osName .. '-' .. machName
+   t.target       = os.getenv("TARGET") or ""
+   t.hostName     = capture("hostname -f"):gsub("%s+","")
 
    s_t = t
    return t
