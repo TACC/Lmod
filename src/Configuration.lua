c78614c2a9735f499b5f72fd950a7121aac6b994
Robert McLay <mclay@zaniahgroup.com>
2013-07-01 06:48:26 -0500

    Improving configuration report

--- src/Configuration.lua
+++ src/Configuration.lua
@@ -34,6 +34,8 @@
 
 require("strict")
 
+require("capture")
+require("fileOps")
 require("pairsByKeys")
 local BeautifulTbl = require('BeautifulTbl')
 local Dbg          = require('Dbg')
@@ -41,24 +43,58 @@ local M            = {}
 
 s_configuration   = false
 
+local function locatePkg(pkg)
+   local result = nil
+   for path in package.path:split(";") do
+      local s = path:gsub("?",pkg)
+      local f = io.open(s,"r")
+      if (f) then
+         f:close()
+         result = s
+         break;
+      end
+   end
+   return result
+end
+   
+
 local function new(self)
    local o = {}
    setmetatable(o,self)
    self.__index = self
 
-   local tbl = {}
-   tbl.prefix               = { doc = "Lmod prefix"                               , value = "@PREFIX@",               }
-   tbl.path_to_lua          = { doc = "Path to Lua"                               , value = "@path_to_lua@",          }
-   tbl.path_to_pager        = { doc = "Path to Pager"                             , value = "@path_to_pager@",        }
-   tbl.lmod_settarg_support = { doc = "Supporting Full Settarg Use"               , value = "@lmod_settarg_support@", }
-   tbl.use_dot_files        = { doc = "Using dotfiles"                            , value = "@use_dot_files@",        }
-   tbl.git_version          = { doc = "Lmod git version"                          , value = "@git@",                  }
-   tbl.ancient              = { doc = "How long the user cache file is valid"     , value = "@ancient@",              }
-   tbl.short_time           = { doc = "Cache Build time at which file is written" , value = "@short_time@",           }
-   tbl.prepend_block        = { doc = "Order in which prepends are done"          , value = "@prepend_block@",        }
-   tbl.colorize             = { doc = "Colorize Lmod"                             , value = "@colorize@",             }
-   tbl.pkg                  = { doc = "Pkg Class name"                            , value = Pkg.name() or "unknown",  }
+   local locSitePkg = locatePkg("SitePackage") or "unknown"
 
+   if (locSitePkg ~= "unknown") then
+      local std_sha1 = "2c2c49b67c4b8310273480360552eaa69713a462"
+      
+      local HashSum = "@path_to_hashsum@"
+      if (HashSum:sub(1,1) == "@") then
+         HashSum = findInPath("sha1Sum")
+      end
+      
+      local result = capture(HashSum .. " " .. locSitePkg)
+      result       = result:gsub(" .*","")
+      if (result == std_sha1) then
+         locSitePkg = "standard"
+      end
+   end
+   
+
+   local tbl = {}
+   tbl.prefix          = { doc = "Lmod prefix"                     , value = "@PREFIX@",               }
+   tbl.path_to_lua     = { doc = "Path to Lua"                     , value = "@path_to_lua@",          }
+   tbl.path_to_pager   = { doc = "Path to Pager"                   , value = "@path_to_pager@",        }
+   tbl.settarg_support = { doc = "Supporting Full Settarg Use"     , value = "@lmod_settarg_support@", }
+   tbl.use_dot_files   = { doc = "Using dotfiles"                  , value = "@use_dot_files@",        }
+   tbl.lmod_version    = { doc = "Lmod git version"                , value = "@git@",                  }
+   tbl.ancient         = { doc = "User cache file valid time(sec)" , value = "@ancient@",              }
+   tbl.short_time      = { doc = "Write cache after (sec)"         , value = "@short_time@",           }
+   tbl.prepend_block   = { doc = "Prepend order"                   , value = "@prepend_block@",        }
+   tbl.colorize        = { doc = "Colorize Lmod"                   , value = "@colorize@",             }
+   tbl.pkg             = { doc = "Pkg Class name"                  , value = Pkg.name() or "unknown",  }
+   tbl.sitePkg         = { doc = "Site Pkg location"               , value = locSitePkg,               }
+   tbl.luaV            = { doc = "Lua Version"                     , value = _VERSION,                 }
    o.tbl = tbl
    return o
 end
@@ -73,11 +109,11 @@ end
 function M.report(self)
    local a   = {}
    local tbl = self.tbl
-   a[#a+1]   = {"Name","Description", "Value"}
-   a[#a+1]   = {"----","-----------", "-----"}
+   a[#a+1]   = {"Name", "Value", "Description",}
+   a[#a+1]   = {"----", "-----", "-----------",}
    
    for k, v in pairsByKeys(tbl) do
-      a[#a+1] = {k, v.doc, v.value}
+      a[#a+1] = {k, v.value, v.doc }
    end
 
    local bt = BeautifulTbl:new{tbl=a}
