adb9315e390640b87e9ab6e2a7ca75e56d7189f2
Robert McLay <mclay@zaniahgroup.com>
2013-07-13 16:14:52 -0500

    more version test foo

--- rt/versionTest/mf/a.lua
+++ rt/versionTest/mf/a.lua
@@ -1,4 +1,8 @@
 LmodMessage("5.0rc2: ",versionTest("5.0rc2"),"\n")
+LmodMessage("5.0:    ",versionTest("5.0"),"\n")
+LmodMessage("5.1:    ",versionTest("5.1"),"\n")
+LmodMessage("5.1.0:  ",versionTest("5.1.0"),"\n")
+LmodMessage("5.1.1:  ",versionTest("5.1.1"),"\n")
 if (versionTest(LmodVersion()) > versionTest("5.0")) then
    LmodMessage("(1) Passed Module Test\n")
 end
