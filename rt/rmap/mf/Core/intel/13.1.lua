4e687e3a0ae3a6772e0773d778de31dc8cd05000
Robert McLay <mclay@zaniahgroup.com>
2014-05-20 16:11:37 -0500

    update it to set paths for rmap test

--- rt/rmap/mf/Core/intel/13.1.lua
+++ rt/rmap/mf/Core/intel/13.1.lua
@@ -3,8 +3,11 @@ local fullVersion = myModuleVersion()
 local pkgVersion  = fullVersion:match('(%d+%.%d+)%.?')
 local pkgNameVer  = pathJoin(name,pkgVersion)
 local mroot       = os.getenv("MODULEPATH_ROOT")
+local base        = pathJoin("/opt/apps",name,fullVersion)
 
-prepend_path("MODULEPATH", pathJoin(mroot, "Compiler", pkgNameVer))
+prepend_path("MODULEPATH",      pathJoin(mroot, "Compiler", pkgNameVer))
+prepend_path("PATH",            pathJoin(base,"bin"))
+prepend_path("LD_LIBRARY_PATH", pathJoin(base,"lib"))
 
 family("compiler")
 
