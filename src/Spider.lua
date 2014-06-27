a7bf7332bb658d6801bb96a30d146cdb526faa25
Robert McLay <mclay@zaniahgroup.com>
2014-06-27 15:16:03 -0500

    forcing all to use %.lua$

--- src/Spider.lua
+++ src/Spider.lua
@@ -1012,7 +1012,7 @@ end
 
 function M.dictModules(self, T,tbl)
    for kk,vv in pairs(T) do
-      kk      = kk:gsub(".lua$","")
+      kk      = kk:gsub("%.lua$","")
       tbl[kk] = 0
       if (next(vv.children)) then
          for k, v in pairs(vv.children) do
