0f1d8c58d3dd041c18a77d37953b2a4d5305c719
Robert McLay <mclay@zaniahgroup.com>
2014-09-01 09:24:21 -0500

    moving to TAG_VERSION 5.7.5

--- src/Version.lua
+++ src/Version.lua
@@ -1,12 +1,12 @@
 --module('Version')
 local M={}
-function M.tag()  return "5.7.4.4"   end
+function M.tag()  return "5.7.5"   end
 function M.git()
    local s = "@git@"
    if (s == "@" .. "git@") then s = "" end
    return s
 end
-function M.date() return "2014-08-27 19:26" end
+function M.date() return "2014-09-01 09:24" end
 function M.name()
   local a = {}
   a[#a+1] = M.tag()
