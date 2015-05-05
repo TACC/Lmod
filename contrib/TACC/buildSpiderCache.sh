7e434d3287c6449c2dd6c01bc5882131cc4125f0
Robert McLay <mclay@zaniahgroup.com>
2015-05-05 11:28:26 -0500

    New TACC buildSpiderCache script

--- contrib/TACC/buildSpiderCache.sh
+++ contrib/TACC/buildSpiderCache.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
 # -*- shell-script -*-
 
+# $Id: fstab 1396 2012-08-08 11:32:33Z karl $
+#------------------------------------------------------------------------------
+# Note: this file is managed via LosF
+#
+# You should, in general, not edit this file directly as you will 
+# *lose* the contents during the next sync process. Instead, 
+# edit the template file in your local config directory:
+# 
+# /admin/build/admin/config/const_files/Stampede/login/buildSpiderCache.sh
+#
+# Questions? karl@tacc.utexas.edu
+#
+#------------------------------------------------------------------------------
+
+
 force=
 if [ "$1" = "--force" ]; then
   force="force"
-fi
+fi  
 
 ########################################################################
 # Generate time stamp file:
@@ -21,8 +36,8 @@ writeTS()
   echo "hostName   = $hostName" >> $fn
   echo "lastUpdate = $dateStr"  >> $fn
   echo "timeEpoch  = $epoch"    >> $fn
-}
-
+}  
+  
 
 ########################################################################
 # Read time stamp file for nodeType
@@ -48,40 +63,54 @@ getModifyTime()
 }
 
 ########################################################################
-#  Build the new database
+#  Build the new database 
 
 buildNewDB()
 {
    local DIR=$1
-   local BASEMP=$2
-   local file=$3
+   local tsfn=$2
+   local MPATH=$3
+   local file=$4
    local option=$file
 
    if [ ! -d $DIR ]; then
-     mkdir -p  $DIR
+     mkdir -p $DIR
    fi
 
    local OLD=$DIR/$file.old.lua
    local NEW=$DIR/$file.new.lua
    local RESULT=$DIR/$file.lua
 
-   rm -f $NEW
-   $LMOD_DIR/spider -o $option $BASEMP > $NEW
-   if [ "$?" = 0 -a -f "$NEW" ]; then
+   local OLD_C=$DIR/$file.old.luac_$LuaV
+   local NEW_C=$DIR/$file.new.luac_$LuaV
+   local RESULT_C=$DIR/$file.luac_$LuaV
+
+   rm -f $OLD $NEW
+   $LMOD_DIR/spider --timestampFn $tsfn -o $option $MPATH > $NEW
+   if [ "$?" = 0 ]; then
       chmod 644 $NEW
       if [ -f $RESULT ]; then
         cp -p $RESULT $OLD
       fi
       mv $NEW $RESULT
+
+      luac -o $NEW_C $RESULT
+
+      chmod 644 $NEW_C
+      if [ -f $RESULT_C ]; then
+        cp -p $RESULT_C $OLD_C
+      fi
+      mv $NEW_C $RESULT_C
+
    fi
 }
 
-########################################################################
+######################################################################## 
 #  Make directores and file be world readable
 umask 022
 
 
-########################################################################
+######################################################################## 
 #  Find an lmod that has a version 5 or better
 for i in /opt/apps/lmod/lmod/libexec             \
          /opt/apps/lmod/5.0rc1/libexec           ; do
@@ -109,8 +138,10 @@ nodeType=$(readTS $timeStamp)
 # directories
 if [ -n "$force" -o $a -ge $b ]; then
   if [ "$nodeType" != "build" ]; then
-    buildNewDB $CacheDir  /opt/apps/modulefiles:/opt/modulefiles  moduleT
-    buildNewDB $RmapDir   /opt/apps/modulefiles:/opt/modulefiles  reverseMapT
+    MPATH=/opt/apps/xsede/modulefiles:/opt/apps/modulefiles:/opt/modulefiles
+    buildNewDB $CacheDir  $timeStamp $MPATH  moduleT 
+    buildNewDB $CacheDir  $timeStamp $MPATH  dbT 
+    buildNewDB $RmapDir   $timeStamp $MPATH  reverseMapT
   fi
 fi
 
@@ -122,11 +153,13 @@ if [ "$nodeType" == "master" ]; then
   if [ ! -d $XSEDE_dir ]; then
     mkdir -p $XSEDE_dir
   fi
-  writeTS $nodeType /home1/moduleData/XSEDE/last_update
-  for i in /opt/apps/xsede/modulefiles /share1/apps/teragrid/modulefiles; do
-    if [ -d $i ]; then
-      buildNewDB /home1/moduleData/XSEDE/cacheDir     $i  moduleT
-      buildNewDB /home1/moduleData/XSEDE/reverseMapD  $i  reverseMapT
-    fi
-  done
+  timeStamp=$XSEDE_dir/last_update
+  writeTS $nodeType $timeStamp
+
+  SHARED_DIRS="/home1/apps/intel13/modulefiles"
+
+  buildNewDB $XSEDE_dir/cacheDir     $timeStamp $SHARED_DIRS  moduleT 
+  buildNewDB $XSEDE_dir/cacheDir     $timeStamp $SHARED_DIRS  dbT 
+  buildNewDB $XSEDE_dir/reverseMapD  $timeStamp $SHARED_DIRS  reverseMapT
+
 fi
