4483e9d0240f1ffce35cfa60f24e33a4ae34dd81
Ben McGough <bmcgough@fredhutch.org>
2016-08-10 14:33:20 -0700

    Updating lfs require and adding license

--- contrib/cache_check/cache_check.lua
+++ contrib/cache_check/cache_check.lua
@@ -1,11 +1,45 @@
 #!/usr/bin/env lua
 
+--------------------------------------------------------------------------
+-- Lmod License
+--------------------------------------------------------------------------
+--
+--  Lmod is licensed under the terms of the MIT license reproduced below.
+--  This means that Lmod is free software and can be used for both academic
+--  and commercial purposes at absolutely no cost.
+--
+--  ----------------------------------------------------------------------
+--
+--  Copyright (C) 2008-2014 Robert McLay
+--
+--  Permission is hereby granted, free of charge, to any person obtaining
+--  a copy of this software and associated documentation files (the
+--  "Software"), to deal in the Software without restriction, including
+--  without limitation the rights to use, copy, modify, merge, publish,
+--  distribute, sublicense, and/or sell copies of the Software, and to
+--  permit persons to whom the Software is furnished to do so, subject
+--  to the following conditions:
+--
+--  The above copyright notice and this permission notice shall be
+--  included in all copies or substantial portions of the Software.
+--
+--  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+--  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+--  OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+--  NONINFRINGEMENT.  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+--  BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+--  ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+--  CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+--  THE SOFTWARE.
+--
+--------------------------------------------------------------------------
+
 -- cache_check.lua - intended to be hooked or cron'd
 -- does a quick check of all modules in module_dir for corresponding entry in dbT.lua in system_cache
 -- prints modules from module_dir not in dbT.lua cache on STDOUT, otherwise is quiet
 -- cache_check.lua -c system_cache_dir_containing_dbT.lua -m module_dir
 -- Ben McGough bmcgough@fredhutch.org 8.8.16
-require'lfs'
+local lfs = require("lfs")
 
 cachedir = ""
 moduledir = ""
