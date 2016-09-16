b3fca3ce1064ef7ea34b360cc78242977eabd69e
Robert McLay <mclay@zaniahgroup.com>
2016-09-16 07:35:13 -0500

    do not abspath on module unuse

--- src/cmdfuncs.lua
+++ src/cmdfuncs.lua
@@ -941,7 +941,6 @@ function UnUse(...)
    local arg = pack(...)
    local nodups = true
    for i = 1, arg.n do
-      local v = abspath(arg[i])
       MCP:remove_path{ ModulePath,  v, delim=":", nodups = true}
       MCP:remove_path{ DfltModPath, v, delim=":", nodups = true}
    end
