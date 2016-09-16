c666f3cecdc59bab53c6593f906d561d27cd1135
Robert McLay <mclay@zaniahgroup.com>
2016-09-16 07:44:46 -0500

    fix bug in unuse.  Solves issue #162

--- src/cmdfuncs.lua
+++ src/cmdfuncs.lua
@@ -941,6 +941,7 @@ function UnUse(...)
    local arg = pack(...)
    local nodups = true
    for i = 1, arg.n do
+      local v = arg[i]
       MCP:remove_path{ ModulePath,  v, delim=":", nodups = true}
       MCP:remove_path{ DfltModPath, v, delim=":", nodups = true}
    end
