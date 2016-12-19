a5aa77b197171aa4faff374f40323bef66bb3fcd
Robert McLay <mclay@zaniahgroup.com>
2016-12-19 16:52:01 -0600

    Now report LMOD_SYSTEM_NAME in the configuration report

--- src/Configuration.lua
+++ src/Configuration.lua
@@ -133,7 +133,8 @@ local function new(self)
    local activeTerm        = haveTermSupport() and "true" or colorize("red","false")
    local settarg_support   = cosmic:value("LMOD_FULL_SETTARG_SUPPORT")
    local lmod_colorize     = cosmic:value("LMOD_COLORIZE")
-   local site_name         = cosmic:value("LMOD_SITE_NAME") or "<empty>"
+   local site_name         = cosmic:value("LMOD_SITE_NAME")   or "<empty>"
+   local system_name       = cosmic:value("LMOD_SYSTEM_NAME") or "<empty>"
    local case_ind_sorting  = cosmic:value("LMOD_CASE_INDEPENDENT_SORTING")
    local disable1N         = cosmic:value("LMOD_DISABLE_SAME_NAME_AUTOSWAP")
    local tmod_rule         = cosmic:value("LMOD_TMOD_PATH_RULE")
@@ -203,6 +204,7 @@ local function new(self)
    tbl.settarg     = { k = "Supporting Full Settarg Use"       , v = settarg_support,      }
    tbl.sitePkg     = { k = "Site Pkg location"                 , v = locSitePkg,           }
    tbl.siteName    = { k = "Site Name"                         , v = site_name,            }
+   tbl.sysName     = { k = "System Name"                       , v = system_name,          }
    tbl.spdr_ignore = { k = "Ignore Cache"                      , v = ignore_cache,         }
    tbl.spdr_loads  = { k = "Cached loads"                      , v = cached_loads,         }
    tbl.tm_ancient  = { k = "User cache valid time(sec)"        , v = ancient,              }
