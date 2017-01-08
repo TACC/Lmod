8ef5cf53267ae932563e967cb69f1c7f86f3f4b0
Paul Jähne <paul.jaehne@gmx.de>
2017-01-08 10:59:23 +0100

    added punctuation at the end of sentences
    small corrections ("Sie")

--- messageDir/de.lua
+++ messageDir/de.lua
@@ -4,9 +4,9 @@ return {
      --------------------------------------------------------------------------
      -- LmodError messages
      --------------------------------------------------------------------------
-     e101 = "Kein Programm zum Bestimmen von Hashwerten verfügbar (sha1sum, shasum, md5sum oder md5)",
+     e101 = "Kein Programm zum Bestimmen von Hashwerten verfügbar (sha1sum, shasum, md5sum oder md5).",
      e102 = "Nicht einlesbar: \"%{path}\". Abbruch!\n",
-     e103 = "Fehler in LocationT:search()",
+     e103 = "Fehler in LocationT:search().",
      e104 = "%{routine}: Kein Modul für \"%{name}\" gefunden. Das sollte nicht passieren!\n",
      e105 = [==[%{routine}: Systemeinstellungs-Tabelle enthält keine %{location} für: "%{name}".
 Überprüfen Sie die Schreibweise des Namens.
@@ -14,16 +14,16 @@ return {
      e106 = [==[%{routine}: Die validT-Tabelle für %{name} hat keinen Eintrag für: "%{value}".
 Überprüfen Sie die Schreibweise des Namens.
 ]==],
-     e107 = "Nicht auffindbar: \"%{name}\"\n",
-     e108 = "Fehler bei der Vererbung: %{name}\n",
+     e107 = "Nicht auffindbar: \"%{name}\".\n",
+     e108 = "Fehler bei der Vererbung: %{name}.\n",
      e109 = [==[Die Einstellungen verhindern den automatischen Austausch von Modulen mit gleichen Namen. Die geladene Version von "%{oldFullName}" muss entladen werden, bevor eine andere Version geladen werden kann. Verwenden Sie dafür swap wie folgt:
 
   $ module swap %{oldFullName} %{newFullName}
 
 Alternativ können Sie die Umgebungsvariable LMOD_DISABLE_SAME_NAME_AUTOSWAP auf "no" setzen, um den automatischen Austausch von Modulen zu aktivieren.
 ]==],
-     e110 = "Das Kommando 'module avail' kann nicht ausgeführt werden. Die Variable MODULEPATH ist entweder nicht gesetzt oder enthält einen ungültigen Wert",
-     e111 = "%{func}(\"%{name}\") ist ungültig, es wird ein Wert benötigt",
+     e110 = "Das Kommando 'module avail' kann nicht ausgeführt werden. Die Variable MODULEPATH ist entweder nicht gesetzt oder enthält einen ungültigen Wert.",
+     e111 = "%{func}(\"%{name}\") ist ungültig, es wird ein Wert benötigt.",
      e112 = [==[Das Modul "%{name}" kann nicht geladen werden, weil diese Module geladen sind:
   %{module_list}
 ]==],
@@ -41,8 +41,8 @@ Verwende folgendes Kommando, um das Modul zu laden:
 
 Falls Sie weitere Unterstützung brauchen, erstellen Sie ein Support-Ticket.
 ]==],
-     e116 = "Unbekannter Schlüssel: \"%{key}\" in setStandardPaths\n",
-     e117 = "Keine passenden Module gefunden\n",
+     e116 = "Unbekannter Schlüssel: \"%{key}\" in setStandardPaths.\n",
+     e117 = "Keine passenden Module gefunden.\n",
      e118 = [==[Benutzer-Modulsammlung: "%{collection}" existiert nicht.
   "module savelist" zeigt mögliche Werte.
 ]==],
@@ -54,15 +54,15 @@ Falls Sie weitere Unterstützung brauchen, erstellen Sie ein Support-Ticket.
      %{fn}: %{message}
 ]==],
      e122 = "sandbox_registration: Das übergebene Argument ist vom Typ: \"%{kind}\". Es sollte eine Liste sein.",
-     e123 = "uuidgen ist nicht verfügbar, Alternative ist ebenfalls fehlgeschlagen",
-     e124 = "Zeitbegrenzung für Spider-Suche ist abgelaufen\n",
-     e125 = "dbT[sn] schlug fehl für sn: %{sn}\n",
-     e126 = "Hashwert konnte nicht berechnet werden\n",
+     e123 = "uuidgen ist nicht verfügbar, Alternative ist ebenfalls fehlgeschlagen.",
+     e124 = "Zeitbegrenzung für Spider-Suche ist abgelaufen.\n",
+     e125 = "dbT[sn] schlug fehl für sn: %{sn}.\n",
+     e126 = "Hashwert konnte nicht berechnet werden.\n",
 
      --------------------------------------------------------------------------
      -- LmodMessages
      --------------------------------------------------------------------------
-     m401 = "\nLmod hat \"%{oldFullName}\" automatisch durch \"%{newFullName}\" ersetzt\n",
+     m401 = "\nLmod hat \"%{oldFullName}\" automatisch durch \"%{newFullName}\" ersetzt.\n",
 
      --------------------------------------------------------------------------
      -- LmodWarnings
@@ -75,19 +75,19 @@ Um die Sammlung neu anzulegen, laden Sie zuerst die gewünschten Module und füh
 Falls sie die Modulsammlung löschen möchten, verwenden sie:
   $ rm ~/.lmod.d/%{collectionName}
 
-Weitere Informationen finden mit 'module help' oder unter http://lmod.readthedocs.org/
-Die geladenen Module wurden nicht verändert
+Weitere Informationen finden Sie mit 'module help' oder unter http://lmod.readthedocs.org/.
+Die geladenen Module wurden nicht verändert.
 
 ]==],
-     w502 = "Fehler in der Zeile module-version: module-name muss vollqualifiziert sein: %{fullName} ist dies nicht\n",
+     w502 = "Fehler in der Zeile module-version: module-name muss vollqualifiziert sein: %{fullName} ist dies nicht.\n",
      w503 = "MODULEPATH des Systems hat sich geändert: Bitte legen Sie ihre gespeicherten Sammlungen neu an.\n",
      w504 = "Sie haben keine Module geladen, weil die Sammlung \"%{collectionName}\" leer ist!\n",
-     w505 = "Die folgenden Module wurden nicht geladen: %{module_list}\n\n",
+     w505 = "Die folgenden Module wurden nicht geladen: %{module_list}.\n\n",
      w506 = "Keine Sammlung mit dem Namen \"%{collection}\" gefunden.",
-     w507 = "MODULEPATH ist nicht definiert\n",
-     w508 = "Der Name einer Sammlung darf kein `.' enthalten. Bitte vergeben Sie einen anderen Namen für: %{name}",
+     w507 = "MODULEPATH ist nicht definiert.\n",
+     w508 = "Der Name einer Sammlung darf kein `.' enthalten. Bitte vergeben Sie einen anderen Namen für: %{name}.",
      w509 = "Die Bezeichnung 'system' für Sammlungen ist reserviert. Bitte vergeben Sie einen anderen Namen.\n",
-     w510 = [==[Sie versuchen in "%{name}" eine leere Modulsammlung zu speichern. Falls Sie dies wollen, verwenden sie:
+     w510 = [==[Sie versuchen in "%{name}" eine leere Modulsammlung zu speichern. Falls Sie dies wollen, verwenden Sie:
   $ module --force save %{name}
 ]==],
    }
