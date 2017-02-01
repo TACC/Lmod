75477cc2686483409b777ee8774f7bb5b34ad326
Robert McLay <mclay@zaniahgroup.com>
2017-01-31 18:27:30 -0600

    Better setup for the fish shell

--- shells/Fish.lua
+++ shells/Fish.lua
@@ -88,7 +88,7 @@ end
 function Fish.expandVar(self, k, v, vType)
    local lineA       = {}
    v                 = tostring(v):multiEscaped()
-   if (vType == "path") then
+   if (k == "PATH") then
       v = v:gsub(":",'" "')
    end
    lineA[#lineA + 1] = "set "
