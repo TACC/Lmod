25722a7345f626cddfe641f8fa76432f42dde50e
Robert McLay <mclay@zaniahgroup.com>
2017-03-01 07:00:17 -0600

    Remove extra debug printing from use_a2/1.0.lua

--- rt/arch_swap/mf/Common/use_a2/1.0.lua
+++ rt/arch_swap/mf/Common/use_a2/1.0.lua
@@ -1,4 +1,6 @@
-local mpath = os.getenv("MODULEPATH")
-mpath = mpath:gsub("/a1/","/a2/")
-io.stderr:write("MPATH: ",mpath,"\n")
-pushenv("MODULEPATH",mpath)
+if (mode() == "load") then
+   local mpath = os.getenv("MODULEPATH")
+   mpath = mpath:gsub("/a1/","/a2/")
+   io.stderr:write("MPATH: ",mpath,"\n")
+   pushenv("MODULEPATH",mpath)
+end
