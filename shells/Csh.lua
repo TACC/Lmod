fd0acc9f545f904637b027cc2ac085f62f6870f7
Robert McLay <mclay@zaniahgroup.com>
2017-10-18 16:45:47 -0500

    Fix removal of semicolons for Csh

--- shells/Csh.lua
+++ shells/Csh.lua
@@ -62,7 +62,7 @@ function Csh.alias(self, k, v)
    else
       v = v:gsub("%$%*","\\!*")
       v = v:gsub("%$([0-9])", "\\!:%1")
-      v = v:gsub(";%s","")
+      v = v:gsub(";%s$","")
       stdout:write("alias ",k," '",v,"';\n")
       dbg.print{   "alias ",k," '",v,"';\n"}
    end
