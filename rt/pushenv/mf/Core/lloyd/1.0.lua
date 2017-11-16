d2498217ca67bc724f26b491b445a3720378f675
Robert McLay <mclay@zaniahgroup.com>
2017-11-16 15:03:20 -0700

    issue #331: Allow pushenv("FOO", false) to clear "FOO"

---
+++ rt/pushenv/mf/Core/lloyd/1.0.lua
@@ -0,0 +1,1 @@
+pushenv("LLOYD",false)
--- src/MasterControl.lua
+++ src/MasterControl.lua
@@ -266,9 +266,13 @@ function M.pushenv(self, name, value)
       varT[stackName] = Var:new(stackName, v64, nodups, ":")
    end
 
-
-   v   = tostring(value)
-   v64 = encode64(value)
+   if (value == false) then
+      v   = false
+      v64 = "false"
+   else
+      v   = tostring(value)
+      v64 = encode64(value)
+   end
    local priority = 0
 
    varT[stackName]:prepend(v64, nodups, priority)
@@ -303,7 +307,9 @@ function M.popenv(self, name, value)
 
    local v64 = varT[stackName]:pop()
    local v   = nil
-   if (v64) then
+   if (v64 == "false") then
+      v = false 
+   elseif (v64) then
       v = decode64(v64)
    end
    dbg.print{"v: ", v,"\n"}
