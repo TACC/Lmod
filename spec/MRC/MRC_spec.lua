a8c9bd305aee5a01c335f016cfed38baf9f7eaf5
Robert McLay <mclay@zaniahgroup.com>
2018-09-20 16:26:36 -0500

    change test to match new names

--- spec/MRC/MRC_spec.lua
+++ spec/MRC/MRC_spec.lua
@@ -1,5 +1,6 @@
 require("strict")
 
+local dbg          = require("Dbg"):dbg()
 local MRC          = require("MRC")
 local testDir      = "spec/MRC"
 
@@ -9,19 +10,19 @@ describe("Testing MRC class #MRC.",
                function()
                   local mrc = MRC:singleton()
                   local ModA={
-                     {kind="module-version",module_name="/15.0.2",      module_versionA={"15.0","15"}},
-                     {kind="module-version",module_name="intel/14.0.3", module_versionA={"14.0"}},
-                     {kind="module-version",module_name="intel/14.0",   module_versionA={"14"}},
-                     {kind="module-version",module_name="intel/14",     module_versionA={"default"}},
+                     {kind="module_version",module_name="/15.0.2",      module_versionA={"15.0","15"}},
+                     {kind="module_version",module_name="intel/14.0.3", module_versionA={"14.0"}},
+                     {kind="module_version",module_name="intel/14.0",   module_versionA={"14"}},
+                     {kind="module_version",module_name="intel/14",     module_versionA={"default"}},
                   }
                   local defaultV = mrc:parseModA_for_moduleA("intel", ModA)
                   assert.are.equal("intel/14.0.3" , defaultV)
 
                   ModA={
-                     {kind="module-version", module_name="/15.0.2", module_versionA={"15.0","15"}},
-                     {kind="module-version", module_name="/14.0.3", module_versionA={"14.0"}},
-                     {kind="module-version", module_name="/14.0",   module_versionA={"14"}},
-                     {kind="module-version", module_name="/14",     module_versionA={"default"}},
+                     {kind="module_version", module_name="/15.0.2", module_versionA={"15.0","15"}},
+                     {kind="module_version", module_name="/14.0.3", module_versionA={"14.0"}},
+                     {kind="module_version", module_name="/14.0",   module_versionA={"14"}},
+                     {kind="module_version", module_name="/14",     module_versionA={"default"}},
                   }
                   defaultV = mrc:parseModA_for_moduleA("intel", ModA)
                   assert.are.equal("intel/14.0.3" , defaultV)
