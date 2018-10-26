bf2fe95fc05f9f729d489f4c4b2bd968c02f4259
Robert McLay <mclay@zaniahgroup.com>
2018-10-26 15:12:07 -0500

    Fix problem with quote rules for Python

--- shells/R.lua
+++ shells/R.lua
@@ -60,7 +60,7 @@ end
 
 function R.expandVar(self, k, v, vType)
    local lineA = {}
-   v = tostring(v):multiEscaped()
+   v = tostring(v):doubleQuoteString()
    io.stderr:write("v: ",v,"\n")
 
    lineA[#lineA + 1] = 'Sys.setenv("'
