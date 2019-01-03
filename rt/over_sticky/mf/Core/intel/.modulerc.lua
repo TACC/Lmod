4e96d4d83e52a4a001d96cd70e0f2c8a1f90a0ce
Robert McLay <mclay@zaniahgroup.com>
2019-01-03 15:56:15 -0600

    Adding debug printing in .modulerc file

--- rt/over_sticky/mf/Core/intel/.modulerc.lua
+++ rt/over_sticky/mf/Core/intel/.modulerc.lua
@@ -1,6 +1,8 @@
-if os.getenv("RSNT_ARCH") == "avx512" then
-	module_version("intel/2018.3","default")
+if (os.getenv("RSNT_ARCH") == "avx512") then
+   io.stderr:write("Marking intel/2018.3 as default\n")
+   module_version("intel/2018.3","default")
 else 
-	module_version("intel/2016.4","default")
+   io.stderr:write("Marking intel/2016.4 as default\n")
+   module_version("intel/2016.4","default")
 end
 
