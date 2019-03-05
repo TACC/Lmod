c0bac09426e7f3da22660e0d694b6bfeabd9ec0b
Robert McLay <mclay@zaniahgroup.com>
2019-03-05 17:34:38 -0600

    Make the spider produces include help from TCL modulefiles

--- src/Spider.lua
+++ src/Spider.lua
@@ -174,7 +174,7 @@ local function findModules(mpath, mt, mList, sn, v, moduleT)
          shell:echo(concatTbl(b,""))
       end
 
-      loadModuleFile{file=fn, shell=shellNm, reportErr=true, mList = mList}
+      loadModuleFile{file=fn, help=true, shell=shellNm, reportErr=true, mList = mList}
       hook.apply("load_spider",{fn = fn, modFullName = fullName, sn = sn})
       mt:setStatus(sn, "active")
    end
