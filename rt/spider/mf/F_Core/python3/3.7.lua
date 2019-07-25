08216166affdf2792b73347c1fecaa2478401496
Robert McLay <mclay@zaniahgroup.com>
2019-07-24 21:33:22 -0500

    what happens when both providing and a module (abcpy)

---
+++ rt/spider/mf/F_Core/python3/3.7.lua
@@ -0,0 +1,5 @@
+module_provides("abcpy/2.1")
+
+help([[
+This is the help message for python3 3.7
+]])
---
+++ rt/spider/mf/F_Core/python3/3.8.lua
@@ -0,0 +1,5 @@
+module_provides("abcpy/2.2")
+
+help([[
+This is the help message for python3 3.8
+]])
