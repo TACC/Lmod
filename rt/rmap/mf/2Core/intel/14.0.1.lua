f20b3f7defc4174a64acd5abba74a91c4b65a15e
Robert McLay <mclay@zaniahgroup.com>
2019-08-28 17:45:16 -0500

    working on paraview non-DAG problem

---
+++ rt/rmap/mf/2Core/intel/14.0.1.lua
@@ -0,0 +1,15 @@
+local name        = myModuleName()
+local fullVersion = myModuleVersion()
+local pkgVersion  = fullVersion:match('(%d+%.%d+)%.?')
+local pkgNameVer  = pathJoin(name,pkgVersion)
+local mroot       = os.getenv("MODULEPATH_ROOT")
+local appsDir     = os.getenv("APPS_DIR")
+local base        = pathJoin(appsDir,name,fullVersion)
+
+
+prepend_path("PATH",            pathJoin(base,"bin"))
+prepend_path("LD_LIBRARY_PATH", pathJoin(base,"lib"))
+prepend_path("MODULEPATH",      pathJoin(mroot, "2Compiler", pkgNameVer))
+
+family("compiler")
+
