38585bc03e1c9d3aa23f87f6cda941fc175fb974
Robert McLay <mclay@zaniahgroup.com>
2021-01-11 11:57:08 -0600

    Changed from user .modulercfiles to system LMOD_MODULERCFILE

--- rt/oldStdEnv/SitePackage.lua
+++ rt/oldStdEnv/SitePackage.lua
@@ -17,9 +17,20 @@ function load_hook(t)
    local WantedStdEnv = os.getenv("WANTED_STDENV") or "StdEnv/2020"
    if (userName == t.modFullName or WantedStdEnv == t.modFullName ) then return end
 
-   local kind_default = mname:defaultKind()
-   if (kind_default == "user") then
-      LmodMessage("Using StdEnv default in ~/.modulerc* files to make old StdEnv: Consider updating")
+   ------------------------------------------------------------
+   -- mname:defaultKind()  returns:
+   --    none   -> no marked default was set.
+   --    system -> Set by system MODULERCFILE
+   --    user   -> Set by user ~/.modulerc* file.
+   --    marked -> Set by either "default" or .version or .modulerc or .modulerc.lua
+   --              in module directory.
+   local kind_default = mname:defaultKind()  
+   
+
+   if (kind_default == "system") then
+      color_banner("red")
+      LmodMessage("\nThe default for StdEnv is changing on some day in the future ...\n")
+      color_banner("red")
    end
 end
 
