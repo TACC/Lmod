2dcbd716d9f43a0bb5a382bb7c1ce00a00194419
Robert McLay <mclay@zaniahgroup.com>
2021-03-15 20:07:38 -0500

    Debugging sh_to_modulefile test (5): remove /.dotnet/ etc from path

--- rt/sh_to_modulefile/path_both_ends.sh
+++ rt/sh_to_modulefile/path_both_ends.sh
@@ -9,6 +9,22 @@ unset NVM_DIR
 unset XDG_CONFIG_HOME
 
 
+OLD_IFS=$IFS
+IFS=:
+NEW_PATH=
+for dir in $PATH; do
+  if   [[ $dir =~ /.dotnet/ ]]; then
+    :
+  elif [[ $dir =~ /.config/ ]]; then
+    :
+  elif [[ $dir =~ /.local/ ]]; then
+    :
+  else
+    NEW_PATH=$NEW_PATH:$dir
+  fi
+done
+IFS=$OLD_IFS
+
 pathA=( /scratch1/projects/compilers/intel20u0/intelpython3/bin
         /scratch1/projects/compilers/intel20u0/intelpython3/condabin
         /scratch1/projects/compilers/intel20u0/parallel_studio_xe_2020/bin64
--- rt/sh_to_modulefile/tstScript.sh
+++ rt/sh_to_modulefile/tstScript.sh
@@ -8,4 +8,20 @@ unset LMOD_FULL_SETTARG_SUPPORT
 unset NVM_DIR
 unset XDG_CONFIG_HOME
 
-PATH=$testDir/bin:$PATH:$testDir/sbin
+OLD_IFS=$IFS
+IFS=:
+NEW_PATH=
+for dir in $PATH; do
+  if   [[ $dir =~ /.dotnet/ ]]; then
+    :
+  elif [[ $dir =~ /.config/ ]]; then
+    :
+  elif [[ $dir =~ /.local/ ]]; then
+    :
+  else
+    NEW_PATH=$NEW_PATH:$dir
+  fi
+done
+IFS=$OLD_IFS
+
+PATH=$testDir/bin:$NEW_PATH:$testDir/sbin
