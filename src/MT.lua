7639a3c1787f87463cf1208c3eda3b7c8cfb5c6f
Robert McLay <mclay@zaniahgroup.com>
2021-10-26 17:52:11 -0500

    minor change to match comment

--- src/MT.lua
+++ src/MT.lua
@@ -679,7 +679,7 @@ function M.lookup_w_userName(self,userName)
 
    -- Case 3: Partial match?
    local partial_match = ("^"..userName:escape().."/"):gsub('//+','/')
-   local i,j      = fullName:find(partial_match)
+   local i,j           = fullName:find(partial_match)
    if (i) then
       return sn
    end
