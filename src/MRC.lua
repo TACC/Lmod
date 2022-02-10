b0119112f9c9561d0fd75e6b93680a7c735eeb5b
Robert McLay <mclay@zaniahgroup.com>
2022-02-10 12:01:47 -0600

    Issue #560: Add check for isVisible() when showing extensions when doing "module avail"

--- src/MRC.lua
+++ src/MRC.lua
@@ -79,6 +79,7 @@ local l_buildMod2VersionT
 -- @param self A MRC object.
 
 local function l_new(self, fnA)
+   --dbg.start{"MRC l_new(fnA)"}
    local o              = {}
    o.__mpathT           = {}  -- mpath dependent values for alias2modT, version2modT
                               -- and hiddenT.
@@ -94,6 +95,7 @@ local function l_new(self, fnA)
    self.__index = self
 
    l_build(o, fnA or getModuleRCT())
+   --dbg.fini("MRC l_new")
    return o
 end
 
@@ -459,6 +461,10 @@ function M.isVisible(self, modT)
    local name      = modT.fullName
    local fn        = modT.fn
    local isVisible = true
+
+   ------------------------------------------------------------
+   -- Must self:update() to read in .modulerc etc files.
+   self:update()
 
    if (self:getHiddenT(mpathA, name) or self:getHiddenT(mpathA, fn)) then
       isVisible = false
