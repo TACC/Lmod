3362d4a0eb089bc54c0bf9dad909156505d6b2d5
Robert McLay <mclay@zaniahgroup.com>
2022-10-10 16:40:08 -0500

    add depends_on("python3") to intel module

--- rt/depends_on/mf/Issue604/Core/intel/1.0.lua
+++ rt/depends_on/mf/Issue604/Core/intel/1.0.lua
@@ -1,8 +1,7 @@
 family("compiler")
 
--- The message printed by the module whatis command
-whatis("intel")
-
 -- Add second level module
 local mroot = os.getenv("MODULEPATH_ROOT")
 prepend_path("MODULEPATH", pathJoin(mroot, "intel"))
+
+depends_on("python3")
