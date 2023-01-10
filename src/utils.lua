268a16e7f74778ff899f66595a9057851347fa68
Robert McLay <mclay@zaniahgroup.com>
2023-01-10 15:07:26 -0600

    Add "shell" as a dynamic shell determiner

--- src/utils.lua
+++ src/utils.lua
@@ -1094,17 +1094,17 @@ end
 
 function dynamic_shell(shellNm)
    local BaseShell = require("BaseShell") 
+   local success   = false
+   
    if (shellNm ~= "shell") then
       if (BaseShell.isValid(shellNm)) then
          success = true
       end
       return shellNm, success
    end
-
    local ppid      = posix.getpid("ppid")
    local system    = posix.uname("%s")
    local n         = shellNm
-   local success   = false
    if (system == "Linux") then
       n = posix.readlink("/proc/"..ppid.."/exe")
       n = barefilename(n)
