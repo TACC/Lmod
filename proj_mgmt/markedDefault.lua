343cf2f93dd555c8ba188a44fd9ca551661139bf
Robert McLay <mclay@zaniahgroup.com>
2024-05-22 15:50:46 -0500

    Added test code for markedDefaults

--- proj_mgmt/markedDefault.lua
+++ proj_mgmt/markedDefault.lua
@@ -7,8 +7,7 @@ local dbg = require("Dbg"):dbg()
 
 local s_fullNameDfltT = {}
 
-local function l_marked_default(fullName, weight)
-   local t = s_fullNameDfltT
+local function l_marked_default(t, fullName, weight)
    local a = {}
    local n = 0
    for s in fullName:split("/") do
@@ -16,26 +15,25 @@ local function l_marked_default(fullName, weight)
       n = n + 1
    end
 
-   dbg.printT("a",a)
-
    local function l_marked_default_helper(i,n,a,t,weight)
-      dbg.start{"l_marked_default_helper()"}
+      --dbg.start{"l_marked_default_helper(i: ",i,", n: ",n,", weight: ",weight,")"}
       local s = a[i]
       if (not t[s] ) then
          t[s] = {}
       end
       if (i == n) then
          t[s].weight = weight
-         dbg.fini("l_marked_default_helper via end of recursiion")
+         --dbg.fini("l_marked_default_helper via end of recursiion")
          return
       else
-         t[s].tree   = {}
-         l_marked_default_helper(i+1,n,a,t[s].tree)
+         t[s].tree   = t[s].tree or {}
+         l_marked_default_helper(i+1,n,a,t[s].tree, weight)
       end
-      dbg.fini("l_marked_default_helper")
+      --dbg.fini("l_marked_default_helper")
    end
 
    l_marked_default_helper(1,n,a,t,weight)
+   return t
 end      
 
 function main()
@@ -43,21 +41,19 @@ function main()
                   {"compiler/intel/x86/22.1","s"},
                   {"compiler/gcc/14.1",      "s"},
    }
-   local fullA = {{"compiler/intel/x86/24.1","s"},
-   }
-   local fullA = {{"intel/24.1","s"},
-   }
 
    dbg:activateDebug()
    
+   local t = s_fullNameDfltT
+
    for i = 1,#fullA do
       local entry    = fullA[i]
       local fullName = entry[1]
       local weight   = entry[2]
       dbg.print{"fullName: ",fullName, ", weight: ",weight,"\n"}
-      l_marked_default(fullName,weight)
-      dbg.printT("s_fullNameDfltT",s_fullNameDfltT)
+      t = l_marked_default(t, fullName,weight)
    end
+   dbg.printT("s_fullNameDfltT",t)
 
 end
 
