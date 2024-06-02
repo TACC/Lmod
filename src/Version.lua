c561bd6308a308e605432697091ee25736e54e6c
Robert McLay <mclay@zaniahgroup.com>
2024-06-02 10:42:04 -0500

    moving to TAG_VERSION 8.7.38

--- src/Version.lua
+++ src/Version.lua
@@ -9,20 +9,26 @@ function M.branchStr()
    end
    return s == "" and s or "[branch: "..s.."]"
 end
-function M.tag()  return "8.7.37"   end
+function M.tag()  return "8.7.38"   end
 function M.git()
-   local s = "8.7.37"
+   local s = "8.7.38"
    if (s == "@" .. "git@") then s = "" end
    if (s == M.tag()      ) then s = "" end
    return s == "" and s or "("..s..")"
 end
-function M.date() return "2024-03-17 13:11 -06:00" end
+function M.date() return "2024-06-02 10:42 -05:00" end
 function M.name()
   local a = {}
   a[#a+1] = M.tag()
   a[#a+1] = M.git()
   a[#a+1] = M.branchStr()
   a[#a+1] = M.date()
-  return table.concat(a," ")
+  local b = {}
+  for i = 1,#a do
+     if (a[i] ~= "") then
+        b[#b+1] = a[i]
+     end
+  end
+  return table.concat(b," ")
 end
 return M
