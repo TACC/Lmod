6442954b9d5424516aa219d048b3bafe8e93469d
Robert McLay <mclay@zaniahgroup.com>
2024-08-22 14:43:00 -0600

    Check for a found python before trying to use it

--- contrib/TACC/CollectModuleData4Web.sh
+++ contrib/TACC/CollectModuleData4Web.sh
@@ -2,7 +2,7 @@
 # -*- shell-script -*-
 
 ######################################################################
-# find ADMIN_DIR (which is where this shell script is found!)
+# find ADMIN_DIR
 
 SCRIPT_NAME="${BASH_SOURCE[0]:-${(%):-%x}}"
 SCRIPT_DIR=${SCRIPT_NAME%/*}
@@ -21,6 +21,7 @@ if [ ! -x $LMOD_DIR/spider ]; then
     echo "$LMOD_DIR/spider command not found!" '-> Quiting!'
     exit 1
 fi
+   
 
 ########################################################################
 # Make sure that $ADMIN_DIR/softwarePage exists
@@ -40,7 +41,7 @@ done
 BASE_MODULE_PATH=${BASE_MODULE_PATH%:}
 
 ########################################################################
-# find name for python3
+# find name for python3 or fallback to python2
 
 PYTHON=
 for cmd in python3 python python2; do
@@ -53,9 +54,15 @@ done
 
 # Build json software page and make it pretty if possible.
 
-$LMOD_DIR/spider -o softwarePage    $BASE_MODULE_PATH > $ADMIN_DIR/softwarePage/softwarePage.old.json
+$LMOD_DIR/spider -o softwarePage    $BASE_MODULE_PATH > $ADMIN_DIR/softwarePage/softwarePage.old.json 
 
-$PYTHON -mjson.tool $ADMIN_DIR/softwarePage/softwarePage.old.json > $ADMIN_DIR/softwarePage/softwarePage.json 2> /dev/null
+########################################################################
+# If a python was found then use json.tool to make pretty output
+if [ -n "${PYTHON:-}" ]; then
+  $PYTHON -mjson.tool $ADMIN_DIR/softwarePage/softwarePage.old.json > $ADMIN_DIR/softwarePage/softwarePage.json 2> /dev/null
+fi
+
+# Clean up
 if [ -s $ADMIN_DIR/softwarePage/softwarePage.json ]; then
   rm -f $ADMIN_DIR/softwarePage/softwarePage.old.json
 else
