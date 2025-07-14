0187c68083b92c856f7c9a1bc9138fb82646ab1c
Robert McLay <mclay@zaniahgroup.com>
2025-07-14 14:35:50 -0600

    Make dbg.print{} support printing table that have keys that are numbers or booleans.

--- tools/pairsByKeys.lua
+++ tools/pairsByKeys.lua
@@ -34,36 +34,21 @@ local sort = table.sort
 --end
 
 function pairsByKeys (t, f)
-   local a = {}
-   local numA = {}
    local strA = {}
-   local n = 0
    for k in pairs(t) do
-      if (type(k) == "number") then
-         numA[#numA+1] = k
-      else
+      if (type(k) == "string") then
          strA[#strA+1] = k
+      else
+         strA[#strA+1] = tostring(k)
       end
-      n    = n + 1
    end
-   sort(numA, f)
    sort(strA, f)
-
-   for i = 1,#numA do
-      a[i] = numA[i]
-   end
-
-   local j = #numA
-   for i = 1,#strA do
-      j = j + 1
-      a[j] = strA[i]
-   end
 
    local i = 0                -- iterator variable
    local iter = function ()   -- iterator function
                    i = i + 1
-                   if a[i] == nil then return nil
-                   else return a[i], t[a[i]]
+                   if strA[i] == nil then return nil
+                   else return strA[i], t[strA[i]]
                    end
                 end
    return iter
