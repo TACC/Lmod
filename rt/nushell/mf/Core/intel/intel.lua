827ec1d96c9cd59dfec0b4d2cc07a6fe2fd1b6b2
Robert McLay <mclay@zaniahgroup.com>
2025-07-15 10:16:07 -0600

    Working on Nushell and stack vars

--- rt/nushell/mf/Core/intel/intel.lua
+++ rt/nushell/mf/Core/intel/intel.lua
@@ -32,6 +32,8 @@ prepend_path('LD_LIBRARY_PATH','/vol/local/intel/mkl/mkl/lib/lib')
 
 family("compiler")
 
+pushenv( "CC", "icc")
+
 local pkgVersion      = "10.1"
 local pkgName         = "intel"
 local pkgNameVer      = pathJoin(pkgName,pkgVersion)
