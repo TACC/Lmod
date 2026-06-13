bfc2bb5e2694bc454d64fccbd9610345e6c30212
Robert McLay <mclay@zaniahgroup.com>
2026-06-13 07:51:50 -0500

    printing of false instead of nil

--- src/Hub.lua
+++ src/Hub.lua
@@ -83,6 +83,7 @@ end
 -- @param self A Hub object.
 -- @param safe A flag.
 function M.singleton(self, safe)
+   safe = safe ~= nil and safe or false
    dbg.start{"Hub:singleton(safe: ",safe,")"}
    if (not s_hub) then
       s_hub = l_new(self, safe)
